9bf4079b7ccb8cd1591e434fc30069f73d9f31ca
DeadJoe <robert.tokarski@gmail.com>
2021-02-22 14:50:23 +0100

    Fix for handling "zero" values

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="google_spreadsheets_fdw",
-    version="1.0.0",
+    version="1.0.1",
     author="TheDeadJoe",
     description="Multicorn-based PostgreSQL foreign data wrapper for Google Spreadsheets",
     long_description=long_description,
